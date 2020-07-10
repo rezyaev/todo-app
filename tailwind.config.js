0b3b461baafc90133076eb9a938a8e4096ce9c4b
Danil Rezyayev <danil.rezyayev@gmail.com>
2020-07-10 18:33:14 +0300

    add dark mode to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
 	purge: ["./src/**/*.tsx", "./public/index.html"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				dark: { raw: "(prefers-color-scheme: dark)" },
+			},
+		},
 	},
 	variants: {},
 	plugins: [],
